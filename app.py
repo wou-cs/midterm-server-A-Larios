2a02b5ec51170e8f6acb70f4c73284c94e499245
A-Larios <alarios21@mail.wou.edu>
2025-05-04 12:26:14 -0700

    get_calcs now handles non-integers being passed in requests.

--- app.py
+++ app.py
@@ -6,8 +6,11 @@ app = Flask(__name__)
 
 @app.route("/api/calcs/<num>", methods=["GET"])
 def get_calcs(num):
-    num_int = int(num)
-    return {
-        "dec": num_int-1,
-        "hex": hex(num_int)
-    }, 200
+    try:
+        num_int = int(num)
+        return {
+            "dec": num_int-1,
+            "hex": hex(num_int)
+        }, 200
+    except:
+        return "Error when performing calculations.", 400
